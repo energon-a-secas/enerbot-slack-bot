66fce0ec1d674b7f6f7b2d621ea4d1f199c7fb78
Luciano Adonis <luciano.adonis@dxc.com>
2018-10-02 10:13:02 -0300

    Now is real

--- Scripts/rules.rb
+++ Scripts/rules.rb
@@ -1,7 +1,9 @@
 def rules_energon
-  ['0. A robot may not harm humanity, or, by inaction, allow humanity to come to harm.',
-   '1. A robot may not injure a human being or, through inaction, allow a human being to come to harm.',
-   '2. A robot must obey any orders given to it by human beings, except where such orders would conflict with the First Law.',
-   '3. A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.',
-   '4. CLASSIFIED']
-    end
+  <<~RULES
+   0. A robot may not harm humanity, or, by inaction, allow humanity to come to harm.
+   1. A robot may not injure a human being or, through inaction, allow a human being to come to harm.
+   2. A robot must obey any orders given to it by human beings, except where such orders would conflict with the First Law.
+   3. A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.
+   4. CLASSIFIED.
+  RULES
+end
