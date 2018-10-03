9490adb39726a52acf33354d768ae43dcc964ca1
Luciano Adonis <luciano.adonis@dxc.com>
2018-10-02 23:56:44 -0300

    Gardel is going to sign

--- client.rb
+++ client.rb
@@ -40,8 +40,10 @@ client.on :message do |data|
     send_message(data, rules_energon, ':black_square:', 'ENERBOT')
   when 'enerbot pack'then
     send_message(data, pack_energon, ':black_square:', 'ENERBOT')
+  when 'enerbot cuando pagan?'then
+    send_message(data, cuando_pagan, ':black_square:', 'ENERBOT')
   when 'enerbot cuanto para el 18'then
-    send_message(data, cuanto_falta, ':black_square:', 'ENERBOT')
+    send_message(data, cuando_sept, ':black_square:', 'ENERBOT')
   when /^bot/ then
     control(data.text.to_s, data.user.to_s, data.channel.to_s)
     if !$output.nil?
