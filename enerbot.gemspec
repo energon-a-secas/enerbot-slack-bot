dc75aaf43423bf0df7386fc9f2518614a703aa77
Luciano Adonis <luciano.adonis@dxc.com>
2018-10-04 12:15:47 -0300

    Now looks good, please dont read the .gemspec

--- enerbot.gemspec
+++ enerbot.gemspec
@@ -3,7 +3,7 @@ Gem::Specification.new do |spec|
   spec.version       = '1.0'
   spec.authors       = ['ENERTEAM']
   spec.email         = ['sorcerer@energon.rocks']
-  spec.files         = ['lib/enerbot.rb']
+  spec.files         = ['lib/enerbot.rb', 'lib/LERN/date.rb', 'lib/LERN/info.rb', 'lib/LERN/quote.rb']
   spec.summary       = 'Slack slave'
   spec.description   = 'Energon slack slave.'
   spec.homepage      = 'https://energon.cloud/'
---
+++ lib/LERN/quote.rb
@@ -0,0 +1,45 @@
+module Quote
+  def self.advice
+    ['En mi opinión, sí',
+     'Es cierto',
+     'Es decididamente así',
+     'Probablemente',
+     'Buen pronóstico',
+     'Todo apunta a que sí',
+     'Sin duda',
+     'Sí',
+     'Sí - definitivamente',
+     'Debes confiar en ello',
+     'Respuesta vaga, vuelve a intentarlo',
+     'Pregunta en otro momento',
+     'Será mejor que no te lo diga ahora',
+     'No puedo predecirlo ahora',
+     'Concéntrate y vuelve a preguntar',
+     'No cuentes con ello',
+     'Mi respuesta es no',
+     'Mis fuentes me dicen que no',
+     'Las perspectivas no son buenas',
+     'Muy dudoso'].sample
+  end
+
+  def self.benefit
+    ['Tómate la tarde libre, proletario.',
+     '¡4 semanas de vacaciones pagadas!',
+     'Escoje el computador y la silla que quieras.',
+     'Snacks, café, frutas y bebidas libre todos los días todo el día.',
+     'Aguinaldo en septiembre y diciembre.',
+     'Bono anual por metas cumplidas.',
+     'Seguro de salud y dental para ti y tu familia.',
+     'Bono anual sólo por ser un :energon: dev',
+     'Permisos para celebrar y compartir en la :energon: Conf'].sample
+  end
+
+  def self.music
+    %w[https://youtu.be/z5OXON8vIaA
+       https://youtu.be/M-iXzPkgNpw?t
+       https://youtu.be/Q91hydQRGyM?t
+       https://youtu.be/HSh73d3TZcA
+       https://youtu.be/D_P-v1BVQn8
+       https://youtu.be/sFrNsSnk8GM].sample
+  end
+end
