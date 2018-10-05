4482b46871383dbbbfb0e8d92a02a0931cd32ecf
Luciano Adonis <luciano.adonis@dxc.com>
2018-10-05 07:12:09 -0300

    Module for remote connections

--- Scripts/ssh.rb
+++ Scripts/ssh.rb
@@ -1,12 +1,31 @@
-require 'rubygems'
-require 'net/ssh'
+module Remote
+  def self.control(server, usuario, canal)
+    # Plagio de ACL de Bototo
+    require 'rubygems'
 
-HOST = ENV['HOST']
-USER = ENV['USER']
-PASS = ENV['PASS']
+    admins  = 'CODECHANNEL'
+    channel = 'CODECHANNEL'
+    $output = if (admins.include? usuario.to_s) && (channel.include? canal.to_s)
+                case server
+                when /pt/ then
+                  'echo ' + server.to_s.split(/\bbot pt \b/) * ''
+                when /proce/ then
+                  'echo ' + server.to_s.split(/\bbot proce \b/) * ''
+                end
 
-def show(name)
-  Net::SSH.start(HOST, USER, password: PASS) do |ssh|
-    ssh.exec!(name.to_s)
+  end
+  end
+
+  def self.ssh(parsed_command)
+    require 'rubygems'
+    require 'net/ssh'
+
+    ssh_host = ENV['HOST']
+    ssh_user = ENV['USER']
+    ssh_pass = ENV['PASS']
+
+    Net::SSH.start(ssh_host, ssh_user, password: ssh_pass) do |ssh|
+      ssh.exec!(parsed_command.to_s)
+    end
   end
 end
