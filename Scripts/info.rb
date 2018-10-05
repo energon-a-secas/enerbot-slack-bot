9ea7d4ac2a143477167ac2c3407770f11eade4b4
Luciano Adonis <luciano.adonis@dxc.com>
2018-10-05 06:39:22 -0300

    Use of modules and shorter calls to methods

--- Scripts/info.rb
+++ Scripts/info.rb
@@ -1,9 +1,11 @@
-def rules_energon
-  <<~RULES
+module Info
+  def self.rules
+      <<~RULES
    0. A robot may not harm humanity, or, by inaction, allow humanity to come to harm.
    1. A robot may not injure a human being or, through inaction, allow a human being to come to harm.
    2. A robot must obey any orders given to it by human beings, except where such orders would conflict with the First Law.
    3. A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.
    4. CLASSIFIED.
-  RULES
+      RULES
+  end
 end
---
+++ Scripts/quote.rb
@@ -0,0 +1,40 @@
+module Quote
+  def self.advice
+    ['En mi opinión, sí',
+     'Es cierto',
+     'Es decididamente así',
+     'Probablemente',
+     'Buen pronóstico',
+     'Todo apunta a que sí',
+     'Sin duda',
+     'Sí',
+     'Sí - definitivamente',
+     'Debes confiar en ello',
+     'Respuesta vaga, vuelve a intentarlo',
+     'Pregunta en otro momento',
+     'Será mejor que no te lo diga ahora',
+     'No puedo predecirlo ahora',
+     'Concéntrate y vuelve a preguntar',
+     'No cuentes con ello',
+     'Mi respuesta es no',
+     'Mis fuentes me dicen que no',
+     'Las perspectivas no son buenas',
+     'Muy dudoso'].sample
+  end
+
+  def self.benefit
+    ['Tómate la tarde libre, proletario.',
+     '¡4 semanas de vacaciones pagadas!',
+     'Escoje el computador y la silla que quieras.',
+     'Snacks, café, frutas y bebidas libre todos los días todo el día.',
+     'Aguinaldo en septiembre y diciembre.',
+     'Bono anual por metas cumplidas.',
+     'Seguro de salud y dental para ti y tu familia.',
+     'Bono anual sólo por ser un :energon: dev',
+     'Permisos para celebrar y compartir en la :energon: Conf'].sample
+  end
+
+  def self.status
+  ['Trabajo muy duro, como un esclavo... :musical_note:'].sample
+  end
+end
