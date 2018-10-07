879e0ca8d5d7112b4b326ce6bb4929a7ce66ed41
Luciano Adonis <luciano.adonis@dxc.com>
2018-10-07 17:14:47 -0300

    Add new options for retrieve information and new style for Slack requests

--- client.rb
+++ client.rb
@@ -6,7 +6,23 @@ require './Scripts/quote'
 module Fly
   def self.message(data, text, icon, username)
     client = Slack::RealTime::Client.new
-    client.web_client.chat_postMessage channel: data.channel, text: text, icon_emoji: icon, username: username
+    client.web_client.chat_postMessage channel: data.channel,
+                                       text: text,
+                                       icon_emoji: icon,
+                                       username: username
+  end
+
+  def self.response(data, path, attachments, icon, username)
+    read_file = File.read("./Info/#{path}")
+    ex_json = JSON.parse(read_file)
+    attachments = ex_json[attachments]
+    client = Slack::RealTime::Client.new
+    client.web_client.chat_postMessage as_user: true,
+                                       channel: data.channel,
+                                       text: 'Revisando mi BD',
+                                       icon_emoji: icon,
+                                       username: username,
+                                       attachments: attachments
   end
 end
 
@@ -30,24 +46,30 @@ client.on :message do |data|
 
   if bot_channel.include? data.channel
     case data.text
-    when /[Ee]nerbot\s(ayuda|help)$/ then
+    when /^enerbot\s(ayuda|help)$/i then
       Fly.message(data, Info.help, bot_icon, bot_name)
-    when /[Ee]nerbot hola/ then
+    when /^enerbot hola/i then
       Fly.message(data, '¡Hola!', bot_icon, bot_name)
-    when /[Ee]nerbot\s(.*)\s(va|estas)$/ then
+    when /^enerbot\s(.*)\s(va|estas)$/i then
       Fly.message(data, Quote.status, bot_icon, bot_name)
-    when /[Ee]nerbot\s(.*)\s(consejo|pregunta)(.*)?/ then
+    when /^enerbot\s(.*)\s(consejo|pregunta)(.*?)/i then
       Fly.message(data, Quote.advice, bot_icon, bot_name)
-    when /[Ee]nerbot(.*)beneficio/ then
+    when /^enerbot(.*)beneficio/i then
       Fly.message(data, Quote.benefit, bot_icon, bot_name)
-    when /[Ee]nerbot(.*)pack/ then
+    when /^enerbot(.*)pack/i then
       Fly.message(data, Info.pack, bot_icon, bot_name)
-    when /[Ee]nerbot\s(.*)\s(rules|reglas)$/ then
+    when /^enerbot\s(.*)\s(rules|reglas)$/i then
       Fly.message(data, Info.rules, bot_icon, bot_name)
-    when /[Ee]nerbot cu[aá]ndo pagan?/ then
+    when /^enerbot cu[aá]ndo pagan?/i then
       Fly.message(data, Time_to.gardel, bot_icon, bot_name)
-    when /[Ee]nerbot cu[aá]nto para el 18?/ then
+    when /^enerbot cu[aá]nto para el 18?/i then
       Fly.message(data, Time_to.september, bot_icon, bot_name)
+    when /^enerbot info/i then
+      if data.text.include? 'How'
+        Fly.response(data, 'example.json', 'attachments', bot_icon, bot_name)
+      else
+        Fly.response(data, 'enerconf.json', 'attachments', bot_icon, bot_name)
+      end
     when 'self-destruct' then
       if bot_admin.include? data.user
         Fly.message(data, 'Bye', bot_icon, bot_name)
@@ -63,4 +85,5 @@ client.on :message do |data|
     end
   end
 end
+
 client.start!
