9862bb3b1154e5e0ad79c3df0df106fa50bd0b05
Luciano Adonis <luciano.adonis@dxc.com>
2018-10-20 19:39:29 -0300

    Need another beer

--- client.rb
+++ client.rb
@@ -2,13 +2,17 @@ require 'slack-ruby-client'
 require './Scripts/date'
 require './Scripts/info'
 require './Scripts/quote'
+require './Scripts/ssh'
 require './functions'
 
-BOT_ICON = ':energon:'
-BOT_NAME = 'ENERBOT'
+BOT_ICON = ':energon:'.freeze
+BOT_NAME = 'ENERBOT'.freeze
 BOT_ADMINS = ENV['SLACK_USERS']
 BOT_CHANNELS = ENV['SLACK_CHANNELS']
-BOT_TOKEN = ENV['SLACK_TOKEN']
+BOT_TOKEN = ENV['SLACK_API_TOKEN']
+HOST_SSH = ENV['HOST_SSH']
+USER_SSH = ENV['USER_SSH']
+PASS_SSH = ENV['PASS_SSH']
 
 Slack.configure do |config|
   config.token = BOT_TOKEN
@@ -22,17 +26,15 @@ client.on :hello do
 end
 
 client.on :message do |data|
-
   if BOT_CHANNELS.include? data.channel
     case data.text
     when /^enerbot/i then
       Case.bot(data)
     when /^enersay/ then
-      if BOT_ADMINS.include? data.user
-        Case.say(data)
-      else
-        Resp.message(data, 'Meh')
-      end
+      Case.say(data) if BOT_ADMINS.include? data.user
+    when /^enerssh/ then
+      text = Remote.ssh(data)
+      Resp.message(data, text) if BOT_ADMINS.include? data.user
     end
   end
 end
