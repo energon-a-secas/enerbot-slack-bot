57cc0b560b2c49d08d541e5cd6905fa78565c4e7
Luciano Adonis <luciano.adonis@dxc.com>
2018-10-21 22:25:09 -0300

    First Test

---
+++ spec/my_test_spec.rb
@@ -0,0 +1,14 @@
+require './Scripts/date'
+require 'date'
+
+context 'Return' do
+
+  specify 'days left to September 18' do
+    Time_to.september == String
+  end
+
+  specify 'end of the month' do
+    Time_to.gardel == String
+  end
+
+end
