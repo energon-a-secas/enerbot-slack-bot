e2521e29c63a102ec4f71733e5fa3f2c8b3caf0c
Luciano Adonis <luciano.adonis@dxc.com>
2018-10-22 09:09:58 -0300

    Add test for enersay

--- spec/case_spec.rb
+++ spec/case_spec.rb
@@ -1,37 +1,56 @@
 require 'slack-ruby-client'
-require './Scripts/quote'
-require './Scripts/system'
-require './Scripts/date'
+require './scripts/quote'
+require './scripts/system'
+require './scripts/date'
 require './core'
 
+class BotValue
+  BOT_ICON = ':energon:'.freeze
+  BOT_NAME = 'ENERBOT'.freeze
+  BOT_TOKEN = ENV['SLACK_API_TOKEN']
+  BOT_PHRASES = ['enerbot hola',
+                 'enerbot pregunta',
+                 'enerbot beneficio',
+                 'enerbot pack'].freeze
+  BOT_CUSTOM = ['enersay123',
+                'enersay 123',
+                'enersayasd',
+                'enersay asd',
+                'enersay:bam:',
+                'enersay :bam:',
+                'enersay$$'].freeze
+end
+
+module Fake
+  def self.text
+    MESSAGE
+  end
+
+  def self.channel
+    '#bots'
+  end
+end
+
+Slack.configure do |config|
+  config.token = BotValue::BOT_TOKEN
+  config.raise 'Missing token' unless config.token
+end
+
 context 'Parse' do
-  specify 'avoid' do
-    BOT_ICON = ':energon:'.freeze
-    BOT_NAME = 'ENERTEST'.freeze
-    BOT_TOKEN = ENV['SLACK_API_TOKEN']
+  specify 'enerfun' do
 
-    Slack.configure do |config|
-      config.token = BOT_TOKEN
-      config.raise 'Missing ENV[SLACK_API_TOKEN]!' unless config.token
-    end
-
-    module Fake
-      def self.text
-        MESSAGE
-      end
-
-      def self.channel
-        '#bots'
-      end
-    end
-
-    phrases = ['enerbot hola',
-               'enerbot pregunta',
-               'enerbot beneficio',
-               'enerbot pack']
-    phrases.each do |i|
+    BotValue::BOT_PHRASES.each do |i|
       MESSAGE = i
       Case.bot(Fake)
+    end
+  end
+end
+
+context 'Send' do
+  specify 'message' do
+    BotValue::BOT_CUSTOM.each do |i|
+      MESSAGE = i
+    Case.say(Fake)
     end
   end
 end
