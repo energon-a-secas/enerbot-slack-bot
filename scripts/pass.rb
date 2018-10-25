50997cc5e08f1875ce2d86587d183536652ce666
Luciano Adonis <luciano.adonis@dxc.com>
2018-10-25 10:13:37 -0300

    Update pass method

--- scripts/pass.rb
+++ scripts/pass.rb
@@ -2,9 +2,9 @@ module Pass
   def self.gen(data)
     require 'passgen'
     if data.text.include? 'sec'
-      Passgen::generate(:symbols => true, :length => 36, :digits_before => 3)
+      Passgen.generate(symbols: true, length: 36, digits_before: 3)
     else
-      "#{rand(36 ** 36).to_s(36)}!"
+      "#{rand(36**36).to_s(36)}!"
     end
   end
 end
