12512fb2c0c0ecbea691f39138b305cf38c4418f
Luciano Adonis V <luciano.adonisv@gmail.com>
2018-10-26 10:20:22 -0300

    [TRIANGLE] Update TC

--- scripts/tc.rb
+++ scripts/tc.rb
@@ -2,10 +2,17 @@ module Credit
   def self.gen(data)
     require 'credy'
     text = data.text.split[2]
-    puts text
-    puts data
-
-    datos = Credy::CreditCard.generate options = { type: text }
-    'Beta feature ' + datos.to_s
+    tarjetas = %w[mastercard
+                  solo
+                  visa
+                  americanexpress
+                  bankcard]
+    if tarjetas.include? text
+      puts data
+      datos = Credy::CreditCard.generate options = { type: text }
+      'Beta feature ' + datos.to_s
+    else
+      "[WARN] We are not currently supporting #{text}"
+  end
   end
 end
