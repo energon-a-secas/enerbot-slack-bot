03c2e605750a4c1fea4cf42e5c1cb1d8ad0f9cf9
Luciano Adonis <luciano.adonis@dxc.com>
2018-10-27 23:41:08 -0300

    Update with rubocop fix

--- scripts/system.rb
+++ scripts/system.rb
@@ -1,6 +1,7 @@
 module System
   def self.rules
     <<-HEREDOC
+   *RULES:*
    0. A robot may not harm humanity, or, by inaction, allow humanity to come to harm.
    1. A robot may not injure a human being or, through inaction, allow a human being to come to harm.
    2. A robot must obey any orders given to it by human beings, except where such orders would conflict with the First Law.
