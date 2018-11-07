b812163d8f647300f20860d5ad3cf6452558c1d6
ENERGON <42976868+energonrocks@users.noreply.github.com>
2018-11-07 16:23:12 -0300

    Update core.rb

--- core.rb
+++ core.rb
@@ -74,7 +74,7 @@ module Case
     when /analiza/i
       Resp.message(data, Peyo.check(data))
     end
-    when /kick up 4d3d3d3/i then
+    when /kick up 4d3d3d3/i
       Resp.message(data, 'http://i.imgur.com/noJWe.gif')
     end
   end
