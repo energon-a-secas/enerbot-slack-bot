78424c9d8f585f7523011d446c6acd80c40dc721
ymedina <yamil.smc@gmail.com>
2018-11-08 19:18:28 -0300

    fix input handling from core call

--- scripts/lingo.rb
+++ scripts/lingo.rb
@@ -5,20 +5,23 @@ require 'uri'
 module Lingo
   
     def self.translate(data)
-        languages =  {"ingles" => "en", "español" => "es", "frances" => "fr", "portugues" => "pt", "ruso" => "ru",
+        languages =  {"ingles" => "en", "español" => "es", "frances" => "fr", "portugues" => "pt", "ruso" => "ru", "aleman" => "de",
             "chino" => "zh", "japones" => "ja", "italiano" => "it", "argentino" => "es", "chileno" => "es", "brasileño" => "pt"}
 
-        flags =  {"ingles" => ":uk:", "español" => ":es:", "frances" => ":fr:", "portugues" => ":flag-pt:", "ruso" => ":ru:",
+        flags =  {"ingles" => ":uk:", "español" => ":es:", "frances" => ":fr:", "portugues" => ":flag-pt:", "ruso" => ":ru:", "aleman" => ":de:",
             "chino" => ":flag-cn:", "japones" => ":jp:", "italiano" => ":it:", "argentino" => ":ar:", "chileno" => ":flag-cl:", "brasileño" => ":flag-br:"}
 
-        params = data.split(' en ')
-        if languages.keys.include? params[1]
-            url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=es&tl=" + languages[params[1]] + "&dt=t&q=" + URI::encode(params[0])
+        params = data.text.split(' ')
+        toLanguage = params[-1]
+        toTranslate = data.text.split(' en ')[0].split("enerbot como se dice ")[1]
+
+        if languages.keys.include? toLanguage and not toTranslate.to_s.empty?
+            url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=es&tl=" + languages[toLanguage] + "&dt=t&q=" + URI::encode(toTranslate)
             result = Net::HTTP.get(URI(url))
 
             translated = JSON.parse(result)[0][0][0]
             <<~HEREDOC
-                #{translated} #{flags[params[1]]}
+                #{translated} #{flags[toLanguage]}
             HEREDOC
         else
             "https://cdn.memegenerator.es/imagenes/memes/full/22/4/22044287.jpg"
