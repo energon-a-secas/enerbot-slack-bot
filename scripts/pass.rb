aa918e291de7becc553d222acefa54b4ecc835a8
Luciano Adonis <luciano.adonis@dxc.com>
2018-11-18 11:45:08 -0300

    Update with text var

--- scripts/pass.rb
+++ scripts/pass.rb
@@ -1,9 +1,9 @@
 # Module that generates a password and sometimes it's secure
 module Pass
-  def self.gen(data)
+  def self.gen(text)
     require 'passgen'
 
-    if data.text.include? 'sec'
+    if text.include? 'sec'
       Passgen.generate(symbols: true, length: 36, digits_before: 3)
     else
       "#{rand(36**36).to_s(36)}!"
