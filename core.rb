bb89ed11974b3de6ccd0720455fbdb87b9d58d4d
Luciano Adonis <luciano.adonis@dxc.com>
2018-11-18 11:22:41 -0300

    Update variables

--- core.rb
+++ core.rb
@@ -36,61 +36,61 @@ module Case
     if data.text =~ /(fuq|faq|info)/
       Case.events(data)
     else
-      z = case data.text
-          when /\s(hello|hola)$/i
-            '¡Hola!'
-          when /(help|ayuda)/i
-            System.help
-          when /(c[oó]mo est[aá]s)/i
-            Quote.status
-          when /(consejo|pregunta)(.*?)/i
-            Quote.advice
-          when /(.*)beneficio/i
-            Quote.benefit
-          when /pack$/i
-            System.pack
-          when /(rules|reglas)$/i
-            System.rules
-          when /cu[aá]ndo pagan/i
-            Time_to.gardel
-          when /cu[aá]nto para el 18/i
-            Time_to.september
-          when /password/i
-            Pass.gen(data)
-          when /(blockchain|blocchain|blocshain)/i
-            'https://youtu.be/MHWBEK8w_YY'
-          when /(tc)/i
-            Credit.gen(data)
-          when /2fa/i
-            Totp.gen(data)
-          when /random/i
-            Rand.value(data)
-          when /pr[oó]ximo feriado$/i
-            Time_to.holiday_count
-          when /hor[oó]scopo/i
-            Pedro.engel(data)
-          when /dame n[uú]meros para el kino/i
-            Lotery.num
-          when /analiza/i
-            Peyo.check(data)
-          when /(celery|tayne|oyster|wobble|4d3d3d3|flarhgunnstow)/i
-            Celery.load(data)
-          when /c[oó]mo se dice/i
-            Lingo.translate(data)
-          when /resultados kino/
-            Lotery.winnerNums
-          when /(valor acci[óo]n (.*?)$)/i
-            Stock.fetch(data.text)
-          when /qr/i
-            QR.generate(data.text)
-          when /wikipedia/i
-            Vieja.sapear(data)
-          when /vuelo/i
-            Flight.info(data.text)
-          when /clima/i
-            Ivan.torres(data.text)
+      mess = case data.text
+             when /\s(hello|hola)$/i
+               '¡Hola!'
+             when /(help|ayuda)/i
+               System.help
+             when /(c[oó]mo est[aá]s)/i
+               Quote.status
+             when /(consejo|pregunta)(.*?)/i
+               Quote.advice
+             when /(.*)beneficio/i
+               Quote.benefit
+             when /pack$/i
+               System.pack
+             when /(rules|reglas)$/i
+               System.rules
+             when /cu[aá]ndo pagan/i
+               Time_to.gardel
+             when /cu[aá]nto para el 18/i
+               Time_to.september
+             when /password/i
+               Pass.gen(data)
+             when /(blockchain|blocchain|blocshain)/i
+               'https://youtu.be/MHWBEK8w_YY'
+             when /(tc)/i
+               Credit.gen(data)
+             when /2fa/i
+               Totp.gen(data)
+             when /random/i
+               Rand.value(data)
+             when /pr[oó]ximo feriado$/i
+               Time_to.holiday_count
+             when /hor[oó]scopo/i
+               Pedro.engel(data)
+             when /dame n[uú]meros para el kino/i
+               Lotery.num
+             when /analiza/i
+               Peyo.check(data)
+             when /(celery|tayne|oyster|wobble|4d3d3d3|flarhgunnstow)/i
+               Celery.load(data)
+             when /c[oó]mo se dice/i
+               Lingo.translate(data)
+             when /resultados kino/
+               Lotery.winnerNums
+             when /(valor acci[óo]n (.*?)$)/i
+               Stock.fetch(data.text)
+             when /qr/i
+               QR.generate(data.text)
+             when /wikipedia/i
+               Vieja.sapear(data)
+             when /vuelo/i
+               Flight.info(data.text)
+             when /clima/i
+               Ivan.torres(data.text)
           end
-      Resp.message(data, z)
+      Resp.message(data, mess)
     end
   end
 
@@ -101,30 +101,30 @@ module Case
           { file: 'meets.json', op1: 'tips' },
           { file: 'contest.json', op1: 'general', op2: 'SDSOS', op3: 'design' }]
 
-    y, z = case data.text
-           when /fuq/
-             [0, :op1]
-           when /faq/
-             [0, :op2]
-           when /eventos$/
-             [1, :op1]
-           when /talks$/
-             [1, :op3]
-           when /tips$/
-             [3, :op1]
-           when /enerlive$/
-             [1, :op2]
-           when /institute$/
-             [2, :op2]
-           when /contest general_info$/
-             [4, :op1]
-           when /contest SDSOS$/
-             [4, :op2]
-           when /contest diseña$/
-             [4, :op3]
+    file, info = case data.text
+                 when /fuq/
+                   [0, :op1]
+                 when /faq/
+                   [0, :op2]
+                 when /eventos$/
+                   [1, :op1]
+                 when /talks$/
+                   [1, :op3]
+                 when /tips$/
+                   [3, :op1]
+                 when /enerlive$/
+                   [1, :op2]
+                 when /institute$/
+                   [2, :op2]
+                 when /contest general_info$/
+                   [4, :op1]
+                 when /contest SDSOS$/
+                   [4, :op2]
+                 when /contest diseña$/
+                   [4, :op3]
            end
 
-    Resp.event(data, as[y][:file], as[y][z])
+    Resp.event(data, as[file][:file], as[file][info])
   end
 
   def self.say(data)
