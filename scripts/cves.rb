06ec129fec4a432fd36c04de77023ba8e8b73339
Luciano Adonis <luciano.adonis@dxc.com>
2018-11-20 13:13:16 -0300

    [TRI] Update with fix.

--- scripts/cves.rb
+++ scripts/cves.rb
@@ -5,11 +5,16 @@ module CVE
         require 'net/http'
         require 'uri'
 
+        num = 5
+        if (match = data.match(/cve list (.*?)$/i))
+            num = match.captures[0]
+        end
+
         result = JSON.parse(Net::HTTP.get(URI('https://cve.circl.lu/api/last')))
 
         message = ''
         result&.each_with_index { |x, index|
-            if index == 10 then break end
+            if index == num.to_i then break end
             message += "*#{x['id']}*:\n> _#{x['summary']}_ \n"   
         }
 
