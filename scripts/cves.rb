e49de871f6b2728eb4462323636339d2211050d2
Luciano Adonis <luciano.adonis@dxc.com>
2018-11-22 09:24:48 -0300

    Ultra fix

--- scripts/cves.rb
+++ scripts/cves.rb
@@ -1,27 +1,30 @@
 # Module to list CVE (valuable security)
 module CVE
-    def self.latest(data)
-        require 'json'
-        require 'net/http'
-        require 'uri'
+  def self.latest(data)
+    require 'json'
+    require 'net/http'
+    require 'uri'
 
-        num = 5
-        if (match = data.match(/cve list (.*?)$/i))
-            num = Integer(match.captures[0]) rescue num
-        end
-
-        result = JSON.parse(Net::HTTP.get(URI('https://cve.circl.lu/api/last')))
-
-        message = ''
-        result&.each_with_index { |x, index|
-            if index == num.to_i then break end
-            message += "*#{x['id']}*:\n> _#{x['summary']}_ \n"   
-        }
-        
-        <<-HEREDOC
-            #{message}
-        HEREDOC
-    
+    num = 5
+    if (match = data.match(/cve list (.*?)$/i))
+      num = begin
+              Integer(match.captures[0])
+            rescue StandardError
+              num
+            end
     end
 
+    result = JSON.parse(Net::HTTP.get(URI('https://cve.circl.lu/api/last')))
+
+    message = ''
+    result.each_with_index do |x, index|
+      break if index == num.to_i
+
+      message += "*#{x['id']}*:\n> _#{x['summary']}_ \n"
+    end
+
+    <<-HEREDOC
+            #{message}
+    HEREDOC
+  end
 end
