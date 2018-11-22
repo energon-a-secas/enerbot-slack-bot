e1fd5944ad2a071de80cc378a38a13100fbad63a
Luciano Adonis <luciano.adonis@dxc.com>
2018-11-22 09:11:49 -0300

    Finally we get to just 1 error, take that rubocop

--- scripts/tc.rb
+++ scripts/tc.rb
@@ -8,8 +8,8 @@ module Credit
     end
     tarjetas = %w[mastercard solo visa bankcard]
     if tarjetas.include? type
-      data = Credy::CreditCard.generate options = { type: type }
-      p ":credit_card:*Type:* #{type}\n:id:*Number:* #{data[:number]}\n:desert_island:*Country:* #{data[:country]}"
+      data = Credy::CreditCard.generate type: type
+      ":credit_card:*Type:* #{type}\n:id:*Number:* #{data[:number]}\n:desert_island:*Country:* #{data[:country]}"
     else
       "[WARN] We are not currently supporting #{text}"
     end
