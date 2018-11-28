9cccc8461264f2ae8597dc1d9b6d3a3dcf250445
Luciano Adonis <luciano.adonis@dxc.com>
2018-11-27 22:28:11 -0300

    Rubocop Fix

--- client_helper.rb
+++ client_helper.rb
@@ -8,8 +8,8 @@ module Unauthorized
   def self.chan(data)
     ":newalert: <@#{data.user}> almost make me work on <##{data.channel}>!"
   end
+
   def self.kill(data)
     "<@#{data.user}> tried to kill me!"
   end
-
 end
