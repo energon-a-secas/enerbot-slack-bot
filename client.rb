5aec166d409090fb773348b22aa046d777009ebc
Luciano Adonis <luciano.adonis@dxc.com>
2018-11-27 23:11:00 -0300

    Change the class name to AccessEval, add new methods under AE and i'm still missing the _helper

--- client.rb
+++ client.rb
@@ -19,11 +19,10 @@ require './scripts/flight'
 require './scripts/weather'
 require './scripts/cves'
 require './scripts/canitrot'
-require './client_helper'
 require './core'
 
 # Variables and other herbs
-class BotValue
+class AccessEval
   BOT_ICON = ':energon:'.freeze
   BOT_NAME = 'ENERBOT'.freeze
   BOT_ADMINS = ENV['SLACK_USERS']
@@ -31,19 +30,31 @@ class BotValue
   BOT_TOKEN = ENV['SLACK_API_TOKEN']
 
   def self.chan(data)
-    ":newalert: <@#{data.user}> almost make me work on <##{data.channel}>!"
+    chan = data.channel
+    if !AccessEval::BOT_CHANNELS.include? chan
+      Resp.message(LERN,":newalert: <@#{data.user}> almost make me work on <##{chan}>!")
+    else
+      Case.bot(data)
+    end
   end
 
   def self.kill(data)
-    "<@#{data.user}> tried to kill me!"
+    user = data.user
+    if !AccessEval::BOT_ADMINS.include? user
+      Resp.message(LERN, ":newalert: <@#{user}> tried to kill me!")
+    else
+      Resp.message(data, Case.kill(data)) && abort('bye')
+    end
+
   end
 
+  # Just for the sake of messaging on start
   def self.channel
     '#bots'
   end
 end
 
-# Just for the sake of messaging on start
+# LERN TECH
 class LERN
   def self.channel
     '#bots'
@@ -51,41 +62,27 @@ class LERN
 end
 
 Slack.configure do |config|
-  config.token = BotValue::BOT_TOKEN
+  config.token = AccessEval::BOT_TOKEN
   config.raise 'Missing ENV[SLACK_API_TOKEN]!' unless config.token
 end
 
 client = Slack::RealTime::Client.new
 
 client.on :hello do
-  Resp.message(LERN, 'Beginning LERN sequence')
+  Resp.message(AccessEval, 'Beginning LERN sequence')
 end
 
 client.on :message do |data|
   text = data.text
   case text
   when /^enerbot/i then
-    if !BotValue::BOT_CHANNELS.include? data.channel
-      Resp.message(BotValue, BotValue.chan(data))
-    else
-      Case.bot(data)
-    end
+    AccessEval.chan(data)
   when /^enersay/ then
     Case.say(data)
   when /^enerssh/ then
     Resp.message(data, Remote.ssh(data))
   when /(enershut|お前もう死んでいる)/ then
-    kill_type = case text
-                when 'enershut'
-                  System.kill
-                when 'お前もう死んでいる'
-                  Quote.japanese
-                end
-    if BotValue::BOT_ADMINS.include? data.user
-      Resp.message(data, kill_type) && abort('bye')
-    else
-      Resp.message(BotValue, BotValue.kill(data))
-    end
+    AccessEval.kill(data)
   end
 end
 client.start!
