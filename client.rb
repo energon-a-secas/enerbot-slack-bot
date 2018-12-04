edef76e58d6cc687b1c82431299b1a6b6b946735
Luciano Adonis V <luciano.adonisv@gmail.com>
2018-12-04 16:33:58 -0300

    Update with new icon

--- client.rb
+++ client.rb
@@ -25,7 +25,7 @@ require './core'
 
 # Variables and other herbs
 class AccessEval
-  BOT_ICON = ':energon:'.freeze
+  BOT_ICON = ':enerbot:'.freeze
   BOT_NAME = 'ENERBOT'.freeze
   BOT_ADMINS = ENV['SLACK_USERS']
   BOT_CHANNELS = ENV['SLACK_CHANNELS']
