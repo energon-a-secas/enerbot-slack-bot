33f32912db4b8b5538a92778bace463209d90d6d
ENERGON <42976868+energonrocks@users.noreply.github.com>
2018-12-10 14:59:07 -0300

    Update secret_friend.rb

--- scripts/secret_friend.rb
+++ scripts/secret_friend.rb
@@ -10,7 +10,7 @@ module SecretFriend
       (0..(friend_list.length - 2)).each do |index|
         message += "*#{friend_list[index]}* regala a *#{friend_list[index + 1]}*, "
       end
-      message += "*#{friendList[friendList.length - 1]}* regala a *#{friendList[0]}*.\n"
+      message += "*#{friend_list[friend_list.length - 1]}* regala a *#{friend_list[0]}*.\n"
       message += "Monto máximo sugerido para regalo: $#{rand(5_000..10_000)}"
     else
       message = 'Al menos deben ser 3 amigos... No creo que sea tan difícil ¿o no?'
