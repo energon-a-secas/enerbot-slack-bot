9b2c6200d4d9ede23354840c6b43109eb3a60499
Luciano Adonis <luciano.adonis@dxc.com>
2019-01-20 23:13:23 -0300

    Final mix, needs a lot of improvement but i will be awesome, eventually

--- client.rb
+++ client.rb
@@ -30,55 +30,51 @@ require './scripts/chimuelo'
 require './core'
 require './system'
 
-# Class that evaluates if your worthy of calling the bo
-class AccessEval
-  BOT_ICON = ENV['SLACK_ICON']
-  BOT_NAME = ENV['SLACK_NAME']
-  BOT_TOKEN = ENV['SLACK_API_TOKEN']
-  BOT_LOG = ENV['SLACK_LOG_CHANNEL']
-  THREAD_REGISTRY = ''
+BOT_TOKEN = ENV['SLACK_API_TOKEN']
+BOT_LOG = ENV['SLACK_LOG_CHANNEL']
 
-  # Return threads id number
-  def self.thread(info)
-    Resp.write(BOT_LOG, info.to_s)
+# Future Gem
+class Enerbot
+  extend Registry
+
+  def initialize
+
+    # Slack Token configure
+    Slack.configure do |config|
+      config.token = BOT_TOKEN
+      config.raise 'Missing ENV[SLACK_API_TOKEN]!' unless config.token
+    end
+
+    # Client initialization
+    client = Slack::RealTime::Client.new
+
+    client.on :hello do
+      Resp.message(BOT_LOG, 'Beginning LERN sequence')
+    end
+
+    # Listen to new messages
+    client.on :message do |data|
+      #user = data.user
+      chan = data.channel
+      text = data.text
+
+      Enerbot.save(data)
+
+      # First layer of case
+      case text
+      when /^enerbot/i then
+        client.typing channel: chan
+        Reply.new(data, text)
+      when /^enersay/ then
+        Reply.new(data, text)
+      when /^enershut/ then
+        Reply.new(data, text)
+      end
+    end
+
+    client.start!
   end
+
 end
 
-# Slack Token configure
-Slack.configure do |config|
-  config.token = AccessEval::BOT_TOKEN
-  config.raise 'Missing ENV[SLACK_API_TOKEN]!' unless config.token
-end
-
-# Client initialization and first message
-client = Slack::RealTime::Client.new
-client.on :hello do
-  Resp.message(AccessEval::BOT_LOG, 'Beginning LERN sequence')
-end
-
-# Endless loop of cases
-client.on :message do |data|
-  user = data.user
-  chan = data.channel
-  text = data.text
-  thread = data.thread_ts
-  registry = AccessEval::THREAD_REGISTRY
-
-  registry << "\n*Channel:* #{chan}, *Thread:* #{thread}, *User:* <@#{user}>, *Text:* #{text}" unless thread.nil? && user != 'enerbot' && !text.to_s.include?('enerbot')
-
-  # Initialization of the big case based on the first word
-  case text
-  when /^enerbot/i then
-    client.typing channel: chan
-    Reply.new(data, text)
-  when /^enersay/ then
-    Reply.new(data, text)
-  when /^enerthread/ then
-    AccessEval.say(user, text)
-  when /^enershut/ then
-    Reply.new(data, text)
-  when /^enerssh/ then
-    AccessEval.thread(registry)
-  end
-end
-client.start!
+Enerbot.new
