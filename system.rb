f1bbcd80154323bc7f3443ae3cfff6e3816d9312
Luciano Adonis <luciano.adonis@dxc.com>
2019-01-20 23:13:31 -0300

    Final mix, needs a lot of improvement but i will be awesome, eventually

--- system.rb
+++ system.rb
@@ -1,19 +1,27 @@
 # Validates origin and permissions over request
 
+BOT_ADMINS = ENV['SLACK_USERS']
+BOT_CHANNELS = ENV['SLACK_CHANNELS']
+BOT_ICON = ENV['SLACK_ICON']
+BOT_NAME = ENV['SLACK_NAME']
+BAN_LIST = ''
+
 module Registry
   def save(data)
     user = data.user
     chan = data.channel
     text = data.text
     thread = data.thread_ts
-    registry << "\n*Channel:* #{chan}, *Thread:* #{thread}, *User:* <@#{user}>, *Text:* #{text}" unless thread.nil? && user != 'enerbot' && !text.to_s.include?('enerbot')
+    @thread = ''
+    @thread << "\n*Channel:* #{chan}, *Thread:* #{thread}, *User:* <@#{user}>, *Text:* #{text}" unless thread.nil? && user != 'enerbot' && !text.to_s.include?('enerbot')
+  end
+
+  def thread
+    p @thread
   end
 end
 
 module Validate
-  BOT_ADMINS = ENV['SLACK_USERS']
-  BOT_CHANNELS = ENV['SLACK_CHANNELS']
-  BAN_LIST = ''.freeze
 
   # Validates if user is banned
   def worthy?(user)
@@ -39,10 +47,58 @@ module Validate
     when 'NOT'
       Resp.message(channel, 'You are still banned until i forget it')
     when 'COMMON'
-      Resp.message('#bots', "User <@#{user}> tried to do something nasty")
+      Resp.message(BOT_LOG, "User <@#{user}> tried to do something nasty")
     when 'LOCKED ORIGIN'
-      Resp.message('#bots', "User <@#{user}> making me work on <##{channel}|#{channel}>")
+      Resp.message(BOT_LOG, "User <@#{user}> making me work on <##{channel}|#{channel}>")
       nil
+    end
+  end
+end
+
+# Works somehow
+module Resp
+  def self.message(data, text, attach = '')
+    puts data
+    thread = data.ts if data.to_s.include?('thread_ts')
+
+    find = if attach != ''
+             json_file = File.read("./Info/#{text}")
+             text = ':energon_enterprise:'
+             JSON.parse(json_file)[attach]
+           else
+             []
+           end
+
+    channel = if data.respond_to? :channel
+                data.channel
+              else
+                data
+              end
+
+    client = Slack::RealTime::Client.new
+    client.web_client.chat_postMessage channel: channel,
+                                       text: text,
+                                       icon_url: BOT_ICON,
+                                       username: BOT_NAME,
+                                       thread_ts: thread,
+                                       attachments: find
+  end
+
+  def self.write(data, text, thread = '')
+    puts data
+    client = Slack::RealTime::Client.new
+    client.web_client.chat_postMessage channel: data,
+                                       text: text,
+                                       icon_url: BOT_ICON,
+                                       username: BOT_NAME,
+                                       thread_ts: thread
+  end
+
+  def self.say(text)
+    if (match = text.match(/enersay (\<[#@])?((.*)\|)?(.*?)(\>)? (.*?)$/i))
+      [match.captures[2] || match.captures[3], match.captures[5]]
+    else
+      ['#bots', 'Meh']
     end
   end
 end
@@ -70,7 +126,7 @@ class Reply
         Resp.message(chan, message) if Reply.redirect(admin, user, channel).nil?
       end
     else
-      Resp.message('#bots', reply) unless Reply.redirect(scope, user, channel).nil?
+      Resp.message(BOT_LOG, reply) unless Reply.redirect(scope, user, channel).nil?
       Resp.message(data, Case.bot(data)) if Reply.redirect(access, user, channel).nil?
     end
   end
