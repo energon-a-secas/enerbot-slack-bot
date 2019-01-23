d18ef5ac6e2f9ac4701b86c14b1cf29d13fb6c44
Luciano Adonis <luciano.adonis@dxc.com>
2019-01-22 21:23:34 -0300

    More dices, more devices

--- scripts/dice.rb
+++ scripts/dice.rb
@@ -1,18 +1,37 @@
 # Tira un dado
 module Roleo
   def self.dados(text)
-    require 'easy_dice'
-    dado = '1d20'
-    if (match = text.match(/roll (.*?)$/i))
-      dado = match.captures[0]
-     end
-    d = begin
-       EasyDice.read(dado)
-        rescue StandardError
-          EasyDice.read('1d20')
-     end
-    <<-HEREDOC
-		#{d.roll}
-    HEREDOC
+    if (match = text.match(/roll (\d+) (\d+)/i))
+      dice = match.captures[0].to_i
+
+      number = match.captures[1].to_i
+      face = if number.even?
+               results = ''
+               p number
+             else
+               results = "You number #{number.to_s} has been increased by 1 :gandalf:\n"
+               p number.next
+             end
+    end
+
+    total = 0
+    min = ''
+    max = ''
+
+
+    if dice > 20
+      ":game_die: #{dice} es un cantidad absurda, prueba con otra"
+    else
+      (1..dice).each do |i|
+        result = (1..face).to_a
+        min = result.min
+        max = result.max
+        num = result.shuffle![3]
+        total += num
+        results += "\n*:game_die: Dado #{i}:* #{num}"
+      end
+      p "*Results:* #{results}\nSuming a total of *#{total}* :gandalf_parrot: \n*Minimum rolled number:* #{min}\n*Maximum rolled number:* #{max} "
+    end
+
   end
 end
