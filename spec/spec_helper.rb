ac0d737affe082db3ede9c94a6317fe10435e468
Luciano Adonis <luciano.adonis@dxc.com>
2019-03-26 17:36:20 -0300

    [TEST-69] Add a test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,50 +1,11 @@
 require 'slack-ruby-client'
-require './scripts/date'
-require './scripts/system'
-require './scripts/quote'
-require './scripts/ssh'
-require './scripts/pass'
-require './scripts/tc'
-require './scripts/2fa'
-require './scripts/random'
-require './scripts/horoscopo'
-require './scripts/lotery'
-require './scripts/security_check'
-require './scripts/wikipedia'
-require './scripts/celery'
-require './scripts/lingo'
-require './scripts/stock'
-require './scripts/flight'
-require './scripts/weather'
-require './core'
+require './enerbot'
 
-class AccessEval
-  BOT_ICON = ':energon:'.freeze
-  BOT_NAME = 'ENERBETAS'.freeze
-  BOT_ADMINS = 'user'.freeze
+# Represents a beautiful incoming user message
+class UserData
 
   def self.text
-    [
-      'enerbot hola',
-      'enerbot pregunta',
-      'enerbot como estas',
-      'enerbot beneficio',
-      'enerbot pack',
-      'enerbot rules',
-      'enerbot cuándo pagan',
-      'enerbot cuanto para el 18',
-      'enerbot password',
-      'enerbot password sec',
-      'enerbot 2fa asadasd',
-      'enerbot random 1 2 3',
-      'enerbot horoscopo piscis',
-      'enerbot resultados kino',
-      'enerbot dame numeros para el kino',
-      'enerbot valor accion energon',
-      'enerbot qr hola',
-      'enerbot vuelo LAN122',
-      'enerbot clima'
-    ].sample
+    'hola'
   end
 
   def self.user
@@ -52,6 +13,6 @@ class AccessEval
   end
 
   def self.channel
-    '#bots'
+    '#bot_monitoring'
   end
 end
