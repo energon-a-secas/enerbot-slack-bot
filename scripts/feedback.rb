b3aea703ecda5956cb15ef653e7196c983647bd3
Jose Miguel Lueiza Valenzuela <jose.lueiza@latam.com>
2019-04-11 16:12:36 -0400

    nueva funcionalidad enerbot dame feedback

---
+++ scripts/feedback.rb
@@ -0,0 +1,15 @@
+module Feedback
+  def self.quote
+    quotes = [
+      'Lo hiciste excelente durante el año. *Bajo lo esperado*',
+      'Jugaste fifa todo el año. *Excepcional*, sigue así',
+      'Jugaste magic en la oficina. *Bajo lo esperado*',
+      'Tomaste desayuno en la oficina 2 veces en el año. *Bajo lo esperado*',
+      'Eres parte de la comunidad de seguridad. *Excepcional*
+    ]
+
+    <<-HEREDOC
+    > #{quotes.sample}
+    HEREDOC
+  end
+end
