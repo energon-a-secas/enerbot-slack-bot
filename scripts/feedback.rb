2483f55f16c86b6d776e47701e3657ac77c04d63
Jose Miguel Lueiza Valenzuela <jose.lueiza@latam.com>
2019-04-12 09:34:45 -0400

    nuevas frases

--- scripts/feedback.rb
+++ scripts/feedback.rb
@@ -5,7 +5,11 @@ module Feedback
       'Jugaste fifa todo el año. *Excepcional*, sigue así',
       'Jugaste magic en la oficina. *Bajo lo esperado*',
       'Tomaste desayuno en la oficina 2 veces en el año. *Bajo lo esperado*',
-      'Eres parte de la comunidad de seguridad. *Excepcional*'
+      'Eres parte de la comunidad de seguridad. *Excepcional*',
+      'No incluiste ninguna palabra en ingles en tus frases. *Bajo lo esperado*',
+      'Pusiste solo emojis tristes en slack. *Bajo lo esperado*',
+      'Saliste 2 veces en el latam noticias hablando sobre la importancia de los terraplanistas. *Excepcional*',
+      'Fuiste el mejor en el baile para romper el hielo en el workshop. *Excepcional*'
     ]
 
     <<-HEREDOC
