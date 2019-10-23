14aa04aa811ab9cfb2f74b7cb783068037a6d751
Luciano Adonis V <luciano.adonisv@gmail.com>
2019-10-23 17:17:21 -0300

    Update versus.rb

--- scripts/versus.rb
+++ scripts/versus.rb
@@ -1,6 +1,6 @@
 module Versus
   def self.get
-    character = [':godzilla:', ':olla: :ladle:', ':ru:', ':us:', ':peyo2:', ':enerbot-jalowin:', ':oh_hai_mark:', ':emo-peter2:', ':digital-transformation-expert:', ':danilo:']
+    character = [':godzilla:', ':peyo-blumel:', ':jadue:', ':olla: :ladle:', ':ru:', ':us:', ':peyo2:', ':enerbot-jalowin:', ':oh_hai_mark:', ':emo-peter2:', ':digital-transformation-expert:', ':danilo:']
     selection = character.shuffle!
     "#{selection[1]} :versus: #{selection[2]}"
   end
