b46005da42a57766e340dfbf3f1909ad60df4fe0
Luciano Adonis <luciano.adonis@dxc.com>
2019-11-10 18:48:12 -0300

    Tournament ARC

--- scripts/versus.rb
+++ scripts/versus.rb
@@ -8,8 +8,8 @@ module Versus
   def self.tournament
     character = [':godzilla:', ':peyo-blumel:', ':jadue:', ':olla: :ladle:', ':ru:', ':us:', ':peyo2:', ':enerbot-jalowin:', ':oh_hai_mark:', ':emo-peter2:', ':digital-transformation-expert:', ':danilo:']
     contest = character.shuffle![0..3]
-    show = "Welcome to the Smash of Security\nOur participants are:\n"
-    contest.each { |v| show += "Player: #{v}\n" }
+    show = ":announcer:: Welcome to the Security Smash Tournament :final-smash: Our contestants are:\n"
+    contest.each { |v| show += "- #{v}\n" }
 
     round1 = contest[0..1].sample
     round2 = contest[2..3].sample
@@ -18,9 +18,9 @@ module Versus
     finals << round2
     winner = finals.sample
 
-    show += "First round: #{contest[0]} vs #{contest[1]}\n"
-    show += "Second round: #{contest[2]} vs #{contest[3]}\n"
-    show += "Final round: #{round1} vs #{round2}\n"
-    show += "WINNER: #{winner}"
+    show += "*First round:* #{contest[0]} :versus3: #{contest[1]}\n*Winner:* round1\n"
+    show += "*Second round:* #{contest[2]} :versus3: #{contest[3]}\n*Winner:* round2\n"
+    show += "*Final round:* #{round1} :versus3: #{round2}\n"
+    show += ":crown: *WINNER OF THE TOURNAMENT*: #{winner} "
   end
 end
